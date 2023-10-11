3b988452c7084273451b584d673c1294393aa74b
Mariia Verhun <mariia.verhun@gmail.com>
2023-10-12 01:41:57 +0300

    adding lab1 version 0.1

---
+++ Lab1/encoder/mappings.py
@@ -0,0 +1,15 @@
+mapping_table = {
+    0: 'NNWWN',
+    1: 'WNNNW',
+    2: 'NWNNW',
+    3: 'WWNNN',
+    4: 'NNWNW',
+    5: 'WNWNN',
+    6: 'NWWNN',
+    7: 'NNNWW',
+    8: 'WNNWN',
+    9: 'NWNWN'
+}
+
+start = 'WNNNN'
+stop = 'WNNNN'
